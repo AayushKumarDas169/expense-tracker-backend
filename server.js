fb9d8d2fb59b9edd6dba65186a9e7c3df167e1cc
AayushKumarDas169 <aayushkumardas030405@gmail.com>
2026-05-24 13:21:49 +0530

    configure backend deployment architecture for vercel serverless functions

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
 app.use("/api/savings", require("./routes/savingsRoutes"));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
